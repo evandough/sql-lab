03187ea87ac2e1bd1b4b0e225979919f58cde135
evandough <101676257+evandough@users.noreply.github.com>
2025-04-10 10:56:54 -0500

    Update investigation.sql

--- sql-murder-mystery/investigation.sql
+++ sql-murder-mystery/investigation.sql
@@ -1,1 +1,6 @@
-
+-- We know the murder happened in SQL City on 01/15/2018, we ran this query: 
+SELECT *
+  FROM crime_scene_report
+  WHERE type = 'murder'
+  AND city = 'SQL City'
+  AND date = '20180115'
