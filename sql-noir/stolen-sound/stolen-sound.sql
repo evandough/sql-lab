860219a5bc7756cf1c792046a38acad21856ea6f
evandough <101676257+evandough@users.noreply.github.com>
2025-04-25 11:40:13 -0500

    Create stolen-sound.sql

---
+++ sql-noir/stolen-sound/stolen-sound.sql
@@ -0,0 +1,11 @@
+-- Step 1
+-- Find crime scene information 
+
+SELECT *
+FROM crime_scene
+WHERE location = 'West Hollywood Records'
+
+-- Results
+-- id	  date	    type	  location	                description
+-- 65	  19830715	theft	  West Hollywood Records	  A prized vinyl record was stolen from the store during a busy evening.
+---------------------------------------------------------------------------------------------------------------------------
